f0f4d1fa28df2811fa8f49db009f1ef33b79fc07
Luskalves <lusca.alves32@gmail.com>
2023-02-07 12:43:00 -0300

    ajustes nas models

--- Models/Post.cs
+++ Models/Post.cs
@@ -1,9 +1,12 @@
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 
 public class Post
 {
   [Key]
   public int PostId { get; set; }
   public string Description { get; set; }
+  [ForeignKey("UserId")]
   public int UserId { get; set; }
 }

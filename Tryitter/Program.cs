5f0279a3f06d3f41a1b4fb04c01b1c675a0c4553
Luskalves <lusca.alves32@gmail.com>
2023-02-12 10:34:21 -0300

    Configurações adcionadas ao program

--- Tryitter/Program.cs
+++ Tryitter/Program.cs
@@ -1,8 +1,28 @@
+using Tryitter.Context;
+using Tryitter.Repository;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+var Configuration = new ConfigurationBuilder()
+    .AddJsonFile("appsettings.json")
+    .Build();
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddDbContext<DbContext>(options =>
+{
+    options.UseSqlServer(Configuration.GetConnectionString(@"
+        Server=127.0.0.1;
+        User=SA;
+        Password=Tryitter123;
+        Database=Tryitter;
+        trustServerCertificate=true;
+    "));
+});
+builder.Services.AddScoped<DbContext, TryitterDB>();
+builder.Services.AddScoped<ITryRepository, TryRepository>();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();

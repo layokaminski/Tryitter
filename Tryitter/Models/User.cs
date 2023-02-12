75eb8b8ce2c56871f6d147d2fa8b3de763ea005c
Luskalves <lusca.alves32@gmail.com>
2023-02-12 10:34:07 -0300

    posts do usuário anulavel

--- Tryitter/Models/User.cs
+++ Tryitter/Models/User.cs
@@ -9,6 +9,6 @@ namespace Tryitter.Models
     public string Name { get; set; }
     public string Email { get; set; }
     public string Password { get; set; }
-    public ICollection<Post> Posts { get; set; }
+    public ICollection<Post>? Posts { get; set; }
   }
 }

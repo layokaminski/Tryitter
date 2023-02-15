636425a02ba3fa35038aa73eaf48fe5aad334d15
Luskalves <lusca.alves32@gmail.com>
2023-02-15 12:56:41 -0300

    rota de ultimo post criada

--- Tryitter/Repository/IPostRepository.cs
+++ Tryitter/Repository/IPostRepository.cs
@@ -6,6 +6,7 @@ namespace Tryitter.Repository
   {
     public Task<IEnumerable<Post>> GetAll();
     public Task<IEnumerable<Post>> GetAllById(int id);
+    public Task<Post?> GetLastById(int id);
     public Task<Post?> GetById(int id);
     public Task<Post> Create(Post entity);
     public Task<Post> Update(Post entity, int id);
